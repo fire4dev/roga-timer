e804a4b82f00020a0f2d89630b6a9b3f9779344a
Siderlan Santos <siderlan.santos@gmail.com>
2019-11-25 15:18:58 -0300

    Add new items to the store

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    drawer: false,
+    focusColor: 'red',
+    shortbreakColor: 'light-blue',
+    longbreakColor: 'green'
+  },
+  getters: {
+    drawer (state) {
+      return state.drawer
+    }
   },
   mutations: {
+    DRAWER (state, payload) {
+      state.drawer = payload
+    }
   },
   actions: {
+    setDrawer (context, payload) {
+      context.commit('DRAWER', payload)
+    }
   },
   modules: {
     timer
